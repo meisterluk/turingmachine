5d1240a53dee9912e80b521f63229e9c47bec8c2
meisterluk <admin@lukas-prokop.at>
2013-09-28 14:53:28 +0200

    General development.

--- build.dart
+++ build.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:web_ui/component_build.dart';
 
-// Ref: http://www.dartlang.org/articles/dart-web-components/tools.html
-main() {
+void main() {
   build(new Options().arguments, ['web/turingmachine.html']);
 }
--- web/turingmachine.dart
+++ web/turingmachine.dart
@@ -1,38 +1,23 @@
-import 'machine.dart';
-import 'state.dart';
-import 'table.dart';
-import 'tape.dart';
-import 'movement.dart';
+library turingmachine;
+
+import 'dart:collection';
+import 'dart:html';
+
+part 'lib/machine/machine.dart';
+part 'lib/machine/movement.dart';
+part 'lib/machine/state.dart';
+part 'lib/machine/table.dart';
+part 'lib/machine/tape.dart';
+part 'lib/metadata/todo.dart';
+part 'lib/ui/ui.dart';
 
 const app_name = "turingmachine";
 const app_version = "0.1.0-beta";
 const app_author = "Lukas Prokop <admin@lukas-prokop.at>";
 
-void main() {
-  // Get a turingmachine instance
-  Machine tm = new Machine();
+Application app = new Application(app_name, app_version, app_author);
 
-  // Define a transition table
-  Table tab = new Table();
-  tab.update(0, State.Start, 1, Movement.L, State.End);
-  tab.update(1, State.Start, 0, Movement.L, State.End);
-
-  // Define an empty tape
-  Tape tape = new Tape(2);
-
-  // Initialize turing machine
-  tm.init(State.Start, tab, tape);
-  tm.addFinalState(State.End);
-
-  assert(identical(state("Z1"), state("Z1")));
-  assert(identical(state("HW"), state("HW")));
-
-  // Iterate until some final state is reached
-  for (var symbol in tm.iterator) {
-    print(symbol.toString() + "\n");
-  }
-  print(tape.toString());
-  print(tm.state);
-
-  print("Done.");
+void main()
+{
+  app.run();
 }
